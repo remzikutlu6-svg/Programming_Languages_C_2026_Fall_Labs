914e884daec8d5fc7f72d0d9a3ee8109c5902bef
Remzi Kutlu <remzikutlu6@gmail.com>
2026-09-23 06:33:21 +0000

    Complete Lab 2

--- src/lab2_2.c
+++ src/lab2_2.c
@@ -12,8 +12,12 @@
 */
 
 long long factorial(int n) {
-    // TODO: compute factorial iteratively
-    return 1; // placeholder
+    long long result = 1;
+
+    for (int i = 1; i <= n; i++) {
+        result = result * i;
+    }
+    return result; 
 }
 
 int main(void) {
@@ -22,7 +26,13 @@ int main(void) {
     printf("Enter a non-negative integer n: ");
     scanf("%d", &n);
 
-    // TODO: validate input, call function, print result
+    if (n < 0) {
+        printf("Error\n");
+    } else {
+        long long result = factorial(n);
+        printf("Factorial = %lld\n", result);
+    }
+
 
     return 0;
 }
